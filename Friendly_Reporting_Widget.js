5d1c76786d14ac368172f52578b03b289bf5b767
erda.sheshi@sap.com <erda.sheshi@sap.com>
2024-02-27 09:33:35 +0100

    test styling

--- Friendly_Reporting_Widget.js
+++ Friendly_Reporting_Widget.js
@@ -1,14 +1,13 @@
 (function () {
   let template = document.createElement("template");
   template.innerHTML = `
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
-  <link rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-  <link rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0" />
 
 <style>
     :host {}
+
+    @import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css";
+    @import "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0";
+    @"https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0"
 
     html {
       overflow-y: scroll;
